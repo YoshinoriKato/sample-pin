bc03456ec4d79032b2e60bf0ef4556f8aad491cd
Yoshinori Kato <kato.programmer@gmail.com>
2012-08-20 16:18:33 +0900

    折り返し許可。

--- WebContent/js/autoUrlLink.js
+++ WebContent/js/autoUrlLink.js
@@ -4,13 +4,9 @@
 $.fn.autoUrlLink = function(baseURL) {
 	return this.each(function() {
 		var srcText = this.innerHTML;
-//		srcText = srcText.replace(/(http:\/\/[\x21-\x7e]+)/gi,
-//		"<a href='$1' target='_blank'>$1</a>");
-//		this.innerHTML = srcText.replace(/(https:\/\/[\x21-\x7e]+)/gi,
-//		"<a href='$1' target='_blank'>$1</a>");
 		srcText = srcText.replace(/(http:\/\/[\x21-\x7e]+)/gi,
-		"<a href='$1' target='_blank'>Go to other site.</a>");
+		"<a href='$1' target='_blank'>$1</a>");
 		this.innerHTML = srcText.replace(/(https:\/\/[\x21-\x7e]+)/gi,
-		"<a href='$1' target='_blank'>Go to other site.</a>");
+		"<a href='$1' target='_blank'>$1</a>");
 	});
 };
